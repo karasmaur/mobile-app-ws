1dfa686d00d29a14d3696ed1bd571b8257f6b6b1
mkaras <mkaras@atlassian.com>
2019-01-12 17:34:52 -0200

    Changed UserRepository into a interface(as supposed).

---
+++ src/main/java/com/karas/app/mobileappws/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.karas.app.mobileappws.repository;
+
+import com.karas.app.mobileappws.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findUserByEmail(String email);
+}
--- src/main/java/com/karas/app/mobileappws/service/impl/UserServiceImpl.java
+++ src/main/java/com/karas/app/mobileappws/service/impl/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.karas.app.mobileappws.service.impl;
 
-import com.karas.app.mobileappws.UserRepository;
+import com.karas.app.mobileappws.repository.UserRepository;
 import com.karas.app.mobileappws.io.entity.UserEntity;
 import com.karas.app.mobileappws.service.UserService;
 import com.karas.app.mobileappws.shared.dto.UserDto;
@@ -20,8 +20,10 @@ public class UserServiceImpl implements UserService {
 
         BeanUtils.copyProperties(user, userEntity);
 
+        // TODO: Hardcoded password and id, should be removed later.
         userEntity.setEncryptedPassword("test");
         userEntity.setUserId("testUserId");
+        //
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
 
