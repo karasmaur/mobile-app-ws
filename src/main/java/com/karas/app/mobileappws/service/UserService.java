02add1896c3a339001579b8f52a2840b9e99068d
mkaras <mkaras@atlassian.com>
2019-01-18 00:22:00 -0200

    Added WebSecurity config. Added UserLogin class to receive json payload

--- src/main/java/com/karas/app/mobileappws/service/UserService.java
+++ src/main/java/com/karas/app/mobileappws/service/UserService.java
@@ -1,7 +1,8 @@
 package com.karas.app.mobileappws.service;
 
 import com.karas.app.mobileappws.shared.dto.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto user);
 }
--- src/main/java/com/karas/app/mobileappws/service/impl/UserServiceImpl.java
+++ src/main/java/com/karas/app/mobileappws/service/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.karas.app.mobileappws.shared.dto.UserDto;
 import com.karas.app.mobileappws.shared.dto.Utils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -42,5 +44,10 @@ public class UserServiceImpl implements UserService {
         BeanUtils.copyProperties(storedUserDetails, returnValue);
 
         return returnValue;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
     }
 }
