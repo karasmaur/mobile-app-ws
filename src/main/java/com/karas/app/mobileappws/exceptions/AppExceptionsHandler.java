9cdb2331b0f9fa2541215c1a70eb6465a58c4138
mkaras <mkaras@atlassian.com>
2019-02-12 19:54:51 -0200

    Add method to handle all other exceptions.

--- src/main/java/com/karas/app/mobileappws/exceptions/AppExceptionsHandler.java
+++ src/main/java/com/karas/app/mobileappws/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,6 @@
 package com.karas.app.mobileappws.exceptions;
 
+import com.karas.app.mobileappws.ui.model.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,12 +8,25 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class AppExceptionsHandler {
 
     @ExceptionHandler(value = {UserServiceException.class})
     public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request){
-        return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleOtherExceptions(Exception ex, WebRequest request){
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
